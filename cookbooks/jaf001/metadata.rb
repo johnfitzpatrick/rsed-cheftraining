11aba70dd639b554047c94eadb5472ac2a92fed8
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-01 13:52:02 +0000

    minor edit

--- cookbooks/jaf001/metadata.rb
+++ cookbooks/jaf001/metadata.rb
@@ -5,4 +5,4 @@ description      "This cookbook Configures jaf001"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-recipe "jaf001::default", "jaf001 description"
+recipe "jaf001::default", "jaf001 descriptioni #1"
