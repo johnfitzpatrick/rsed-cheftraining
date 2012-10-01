0a218c80de7272ea569b8cea841ebb4308857aae
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-01 14:04:31 +0000

    used the rake command

--- cookbooks/jaf001/metadata.rb
+++ cookbooks/jaf001/metadata.rb
@@ -1,8 +1,8 @@
 maintainer       "JAF Company"
 maintainer_email "john.fitzpatrick@rightscale.com"
 license          "All rights reserved"
-description      "This cookbook Configures jaf001"
+description      "This cookbook Configures jaf001 v0.1"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-recipe "jaf001::default", "jaf001 descriptioni #1"
+recipe "jaf001::default", "jaf001 descriptioni v0.1"
