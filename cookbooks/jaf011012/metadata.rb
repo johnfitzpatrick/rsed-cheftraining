5362c4754c2fb8bb6d0a16a3b5f6d95e37c18b7d
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-01 10:50:52 +0000

    updated description

--- cookbooks/jaf011012/metadata.rb
+++ cookbooks/jaf011012/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "YOUR_COMPANY_NAME"
 maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
-description      "Installs/Configures jaf011012"
+description      "My cookbook jaf011012"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
