f66c0bffab0d4e75c0b3e2baf28a1d3b77ed2ee1
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-09 16:16:27 +0000

    typo in attributes

--- cookbooks/splunk/attributes/default.rb
+++ cookbooks/splunk/attributes/default.rb
@@ -1,3 +1,3 @@
 default[:splunk][:install]     = "/opt"
 default[:splunk][:user]        = "splunk"
-default[:splunk][:installer]   = "http://www.splunk.com/page/ download_track?file=4.3.4/splunk/linux/splunk-4.3.4-136012-Linux-x86_64.tgz"
+default[:splunk][:installer]   = "http://www.splunk.com/page/download_track?file=4.3.4/splunk/linux/splunk-4.3.4-136012-Linux-x86_64.tgz"
