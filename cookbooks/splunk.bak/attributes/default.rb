a539d45a2dc2b84c9303a33bee0fb4aa5e6e73d1
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-10 16:19:25 +0000

    duplicated splunk cookbook to create sime and more complex versions

---
+++ cookbooks/splunk.bak/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:splunk][:install]     = "/opt"
+default[:splunk][:user]        = "splunk"
+default[:splunk][:installer]   = "https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
