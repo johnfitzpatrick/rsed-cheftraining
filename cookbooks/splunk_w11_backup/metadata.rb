044de7a743661e0edb1c2a8389a74a4fbac86239
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-11 14:10:17 +0000

    updated attributes and recipe

--- cookbooks/splunk_w11_backup/metadata.rb
+++ cookbooks/splunk_w11_backup/metadata.rb
@@ -5,6 +5,5 @@ description      "Installs/Configures splunk_w11"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-depends "rightscale"
-recipe "splunk_w11::default", "Installs splunk from a tar a file - V2"
+recipe "splunk_w11::default", "Installs splunk from a tar a file - V1"
 
---
+++ cookbooks/splunk_w11_backup/recipes/default.rb
@@ -0,0 +1,36 @@
+#
+# Cookbook Name:: splunk_w11
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Installing Splunk - version 1"
+
+user "splunk" do
+action :create
+system true
+shell "/bin/bash"
+end 
+
+
+directory "/opt" do   
+owner "splunk"
+mode "0755"   
+action :create 
+end 
+
+remote_file "/opt/splunk.tar" do   
+source "https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
+action :create_if_missing 
+end 
+
+bash "install_splunk" do
+cwd "/opt"
+code <<-EOH
+tar -xvf splunk.tar
+EOH
+end 
+
