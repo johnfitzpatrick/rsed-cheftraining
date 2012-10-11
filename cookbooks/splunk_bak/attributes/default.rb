e14126e679636aa73b92a1ac286217a3e0af39a5
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-11 10:44:07 +0000

    deleted old files

--- cookbooks/splunk_bak/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default[:splunk][:install]     = "/opt"
-default[:splunk][:user]        = "splunk"
-default[:splunk][:installer]   = "https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
