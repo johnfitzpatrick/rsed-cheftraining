9dfd5816c3a532c27b458a70205e36f0391c48b9
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-12 14:47:33 +0000

    edits

--- cookbooks/splunk_w11/attributes/default.rb
+++ cookbooks/splunk_w11/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:splunk][:install]      = "/opt/splunk"
-default[:splunk][:user]         = "splunk"
-default[:splunk][:installer]    = "http://www.splunk.com/page/download_track?file=4.3.4/splunk/linux/splunk-4.3.4-136012-Linux-x86_64.tgz"
+default[:splunk_w11][:install]      = "/opt/splunk1"
+default[:splunk_w11][:user]         = "splunk"
+default[:splunk_w11][:installer]    = "http://www.splunk.com/page/download_track?file=4.3.4/splunk/linux/splunk-4.3.4-136012-Linux-x86_64.tgz"
 
--- cookbooks/splunk_w11/recipes/install.rb
+++ cookbooks/splunk_w11/recipes/install.rb
@@ -1,4 +1,4 @@
-#
+
 # Cookbook Name:: splunk_w11
 # Recipe:: default
 #
@@ -8,26 +8,26 @@ rightscale_marker :begin
 
 log "installing Splunk"
 
-user node[:splunk][:user] do
+user node[:splunk_w11][:user] do
 action :create
 system true
 shell "/bin/bash"
 end 
 
 
-directory node[:splunk][:install] do   
-owner  node[:splunk][:user]
+directory node[:splunk_w11][:install] do   
+owner  node[:splunk_w11][:user]
 mode "0755"   
 action :create 
 end 
 
-remote_file "#{node[:splunk][:install]}/splunk.tar" do   
-source node[:splunk][:installer]
+remote_file "#{node[:splunk_w11][:install]}/splunk.tar" do   
+source node[:splunk_w11][:installer]
 action :create_if_missing 
 end 
 
 bash "install_splunk" do
-cwd node[:splunk][:install]
+cwd node[:splunk_w11][:install]
 code <<-EOH
 tar -xvf splunk.tar
 EOH
