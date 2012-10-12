c35d6da44084feadef8c8606a82e243e0fb00a90
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-12 12:36:23 +0000

    updated V2

--- cookbooks/splunk_w11/attributes/default.rb
+++ cookbooks/splunk_w11/attributes/default.rb
@@ -1,4 +1,4 @@
-default[:splunk][:install]      = "/opt"
+default[:splunk][:install]      = "/opt/splunk"
 default[:splunk][:user]         = "splunk"
 default[:splunk][:installer]    = "http://www.splunk.com/page/download_track?file=4.3.4/splunk/linux/splunk-4.3.4-136012-Linux-x86_64.tgz"
 
