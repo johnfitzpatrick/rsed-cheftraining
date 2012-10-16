7c896c6e5beb6791edcf43eb33bd0c9f9ec864f5
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-16 11:35:05 +0000

    added hello world

---
+++ cookbooks/hello_world/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: hello_world
+# Recipe:: default
+#
+# Copyright 2012, RightScale <ryan.geyer@rightscale.com>
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello world!"
