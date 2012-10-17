2632829f1dd2c108b2e140f572fa8c5e0db28eb9
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-10-17 11:04:46 +0000

    edited attributes

--- cookbooks/splunk_w11/attributes/default.rb
+++ cookbooks/splunk_w11/attributes/default.rb
@@ -1,4 +1,3 @@
 set[:splunk_w11][:installdir]   = "/opt/splunk"
-default[:splunk_w11][:user]         = "splunk"
 default[:splunk_w11][:installer]    = "https://rs-training-assets.s3.amazonaws.com/splunk-4.3.4-136012-Linux-x86_64.tar"
 
